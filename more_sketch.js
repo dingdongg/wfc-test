3f8a8cb8216300dacec7f93ae892a7c97c29ed17
dingdongg <cs.dg.kim@gmail.com>
2023-07-21 23:55:58 -0700

    create boilerplate for WFC on complex images

---
+++ more_sketch.js
@@ -0,0 +1,25 @@
+const {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
+    BLANK,
+} = require("./sketch");
+
+const constraints = {
+    [UP]: [
+
+    ],
+    [RIGHT]: [
+
+    ],
+    [DOWN]: [
+
+    ],
+    [LEFT]: [
+
+    ],
+    [BLANK]: [
+
+    ],
+};
