0f1f91abbb5aaee345a5e4224dbe3c1430e608a6
dingdongg <cs.dg.kim@gmail.com>
2023-07-21 22:15:19 -0700

    refactor code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const images = {};
 
 const grid = [];
-const DIMENSION = 4;
+let DIMENSION = 104; // max dimensions = 104, before it uses up the entire stack memory with recursions lol
 
 const UP = 1;       //      ㅗ
 const RIGHT = 2;    //      ㅏ
@@ -52,7 +52,7 @@ const constraints = {
 
 const WAVE_STATES = { 0: [] };
 
-function populateNumStates(startNum) {
+function populateWaveStates(startNum) {
     for (let i = 1; i <= startNum; i++) {
         const exponent = Math.floor(Math.log2(i));
         const power = Math.pow(2, exponent);
@@ -93,119 +93,12 @@ function printGridState() {
         const formattedRow = row.map(v => text[v.wave] || v.wave).join("  ").toString();
         ret += `${formattedRow}\n\n`;
     }
-    console.log(ret);
-}
-
-/**
- * returns a random `row` and `offset` index pair for `grid`
- */
-function getRandomGridIndex() {
-    const randomNumber = Math.floor(Math.random() * DIMENSION * DIMENSION);
-    const row = Math.floor(randomNumber / DIMENSION);
-    const offset = randomNumber % DIMENSION;
-
-    return { row, offset };
+    // (ret);
 }
 
 function yieldOneState(wave = WAVE_ARRAY) {
     const randomIndex = Math.floor(Math.random() * wave.length);
     return wave[randomIndex];
-}
-
-// TODO
-function updateNeighborEntropy(row, offset) {
-    console.log(`updateNeighborEntropy(${row}, ${offset})`);
-    // horizontally and vertically
-    if (row < 0 || row >= DIMENSION || offset < 0 || offset >= DIMENSION) {
-        return;
-    }
-
-    const collapsedCell = grid[row][offset];
-
-    const getEntropy = ({ wave }) => WAVE_STATES[wave].length;
-
-    // vertically down a column
-    for (let i = 0; i < DIMENSION; i++) {
-        if (i === row || grid[i][offset].collapsed) continue;
-        printGridState();
-
-        const cell = grid[i][offset];
-        cell.wave &= constraints[collapsedCell.wave][i < row ? UP : DOWN];
-
-        if (getEntropy(cell) === 1) collapse(i, offset);
-        if (getEntropy(cell) === 0) {
-            // look at the immediate neighboring cells (up, right, down, left)
-            const above = (i - 1 >= 0) ? grid[i - 1][offset] : null;
-            const right = (offset + 1 < DIMENSION) ? grid[i][offset + 1] : null;
-            const below = (i + 1 < DIMENSION) ? grid[i + 1][offset] : null;
-            const left = (offset - 1 >= 0) ? grid[i][offset - 1] : null;
-            const adjacent = [above, right, below, left];
-            const bruh = adjacent.reduce((res, adj) => {
-                if (adj?.collapsed) return res & adj.wave;
-                return res;
-            }, INITIAL_WAVE);
-
-            console.log("BRUH", getEntropy({ wave: bruh }));
-
-            if (getEntropy({ wave: bruh}) === 0) {
-                console.log("COL ERROR", cell, `row = ${i}, offset = ${offset}`);
-                // throw new Error ("COL CONTRADICTION");
-                return;
-            } else {
-                cell.wave = bruh;
-            }
-        } 
-    }
-
-    // horizontally across a row
-    for (let j = 0; j < DIMENSION; j++) {
-        if (j === offset || grid[row][j].collapsed) continue;
-        printGridState();
-
-        const cell = grid[row][j];
-        cell.wave &= constraints[collapsedCell.wave][j < offset ? LEFT : RIGHT];
-
-        if (getEntropy(cell) === 1) collapse(row, j);
-        if (getEntropy(cell) === 0) {
-            // look at the immediate neighboring cells (up, right, down, left)
-            const above = (row - 1 >= 0) ? grid[row - 1][j] : null;
-            const right = (j + 1 < DIMENSION) ? grid[row][j + 1] : null;
-            const below = (row + 1 < DIMENSION) ? grid[row + 1][j] : null;
-            const left = (j - 1 >= 0) ? grid[row][j - 1] : null;
-            const adjacent = [above, right, below, left];
-            const bruh = adjacent.reduce((res, adj) => {
-                if (adj?.collapsed) return res & adj.wave;
-                return res;
-            }, INITIAL_WAVE);
-            
-            if (getEntropy({ wave: bruh }) === 0) {
-                console.log("ROW ERROR", cell, `row = ${row}, offset = ${j}`);
-                // throw new Error ("ROW CONTRADICTION");
-                return;
-            } else {
-                cell.wave = bruh;
-            }
-        } 
-    }
-}
-
-function updateAdjacentEntropy(row, offset) {
-    if (row < 0 || row >= DIMENSION || offset < 0 || offset >= DIMENSION) return;
-
-    const collapsed = grid[row][offset];
-    // cell above collapsed cell
-    // cell to the right of collapsed cell
-    // cell below collapsed cell
-    // cell to the left of collapsed cell
-    const above = (row - 1 >= 0) ? grid[row - 1][offset] : null;
-    const right = (offset + 1 < DIMENSION) ? grid[row][offset + 1] : null;
-    const below = (row + 1 < DIMENSION) ? grid[row + 1][offset] : null;
-    const left = (offset - 1 >= 0) ? grid[row][offset - 1] : null;
-
-    above.wave &= constraints[collapsed.wave][UP];
-    right.wave &= constraints[collapsed.wave][RIGHT];
-    below.wave &= constraints[collapsed.wave][DOWN];
-    left.wave &= constraints[collapsed.wave][LEFT];
 }
 
 /**
@@ -215,20 +108,16 @@ function updateAdjacentEntropy(row, offset) {
  * @param {UP | RIGHT | DOWN | LEFT} direction Propagation direction
  */
 function updateNeighbors(row, offset, direction) {
+    // ("updateNeighbors(), direction = ", direction);
     const root = grid[row][offset];
-    let coords;
+    let coords = [row, offset];
 
-    if (direction === UP && (row - 1 >= 0)) {
-        coords = [row - 1, offset];
-    } else if (direction === RIGHT && (offset + 1 < DIMENSION)) {
-        coords = [row, offset + 1];
-    } else if (direction === DOWN && (row + 1 < DIMENSION)) {
-        coords = [row + 1, offset];
-    } else if (direction === LEFT && (offset - 1 >= 0)) {
-        coords = [row, offset - 1];
-    } else return;
+    if (direction === UP && (row - 1 >= 0)) coords[0] -= 1;
+    else if (direction === RIGHT && (offset + 1 < DIMENSION)) coords[1] += 1;
+    else if (direction === DOWN && (row + 1 < DIMENSION)) coords[0] += 1;
+    else if (direction === LEFT && (offset - 1 >= 0)) coords[1] -= 1;
+    else return;
 
-    console.log(coords);
     const cell = grid[coords[0]][coords[1]];
     const newConstraint = constraints[root.wave][direction];
 
@@ -237,12 +126,9 @@ function updateNeighbors(row, offset, direction) {
 
     const newState = WAVE_STATES[cell.wave];
 
-    if (newState.length === 1) cell.collapsed = true;
-
-    updateNeighbors(coords[0], coords[1], direction);
+    if (newState.length === 1) collapse(coords[0], coords[1]);
 }
 
-// TODO
 /**
  * Collapse a given cell
  * 
@@ -251,7 +137,7 @@ function updateNeighbors(row, offset, direction) {
  * @param {number} state Image index
 */
 function collapse(row, offset) {
-    console.log(`collapse(${row}, ${offset})`);
+    // (`collapse(${row}, ${offset})`);
 
     let cell = grid[row][offset];
     const imgIndex = yieldOneState(WAVE_STATES[cell.wave]);
@@ -262,54 +148,11 @@ function collapse(row, offset) {
         image(img, img.width * offset, img.height * row);
     }
 
+    // propagate outwards radially from the root collapsed cell
     updateNeighbors(row, offset, UP);
     updateNeighbors(row, offset, RIGHT);
     updateNeighbors(row, offset, DOWN);
     updateNeighbors(row, offset, LEFT);
-
-    printGridState();
-}
-
-/**
- * Returns the index of a random image
- * @param {any[]} collection
- */
-function getRandomIndex(collection) {
-    return Math.floor(Math.random() * collection.length);
-}
-
-/**
- * Try to insert `n` random images into `n` random places.
- * 
- * Index collisions are ignored, so there may be `<= n` image renders.
- * 
- * @param {number} n Number of inserts
- */
-function insertRandomImages(n) {
-    console.log("insertRandomImages()");
-
-    for (let i = 0; i < n; i++) {
-        const { row, offset } = getRandomGridIndex();
-        collapse(row, offset);
-    }
-}
-
-/**
- * Fill entire `DIMENSION * DIMENSION` grid with random images
- */
-function fillGridWithImages() {
-    for (let i = 0; i < DIMENSION; i++) {
-        for (let j = 0; j < DIMENSION; j++) {
-            const randomIndex = getRandomIndex(images);
-            const img = images[randomIndex];
-            // image(img, img.width * j, img.height * i);
-            collapse(i, j, randomIndex);
-        }
-    }
-}
-
-function renderImages(full, numInserts = DIMENSION) {
-    full ? fillGridWithImages() : insertRandomImages(numInserts);
 }
 
 function getMinEntropyCell() {
@@ -339,30 +182,17 @@ function getMinEntropyCell() {
  * 4. else, 
  */
 function observe() {
-    console.log("observe()");
+    // ("observe()");
     const [row, offset] = getMinEntropyCell();
-
-    console.log(row, offset);
 
     if (row === -1 && offset === -1) return;
 
     collapse(row, offset);
     observe();
-    
-    // const cell = grid[row][offset];
-    // if (cell.wave.length === 0) throw new Error("BROOOOO");
-
-    // const randomState = getRandomIndex(cell.wave);
-    // collapse(row, offset, cell.wave[randomState]);
 }
 
 function waveFunctionCollapse() {
-    console.log("waveFunctionCollapse()");
-
-    let oneDimGrid = [];
-    for (const row of grid) {
-        oneDimGrid = oneDimGrid.concat(row);
-    }
+    // ("waveFunctionCollapse()");
 
     observe();
     // while (uncollapsed.size > 0) {
@@ -387,9 +217,10 @@ function preload() {
 }
 
 function setup() {
-    const imageSize = 128;
+    console.time("TIMING sketch.js");
+    const imageSize = 32;
     const canvasSize = imageSize * DIMENSION;
-    populateNumStates(INITIAL_WAVE);
+    populateWaveStates(INITIAL_WAVE);
 
     createCanvas(canvasSize, canvasSize);
     rect(0, 0, canvasSize, canvasSize);
@@ -398,9 +229,9 @@ function setup() {
 
     initGrid();
     // insertRandomImages(1);
-    // renderImages(false, 1);
-    // printGridState();
     waveFunctionCollapse();
+    console.timeEnd("TIMING sketch.js");
+    printGridState();
 }
 
 function draw() {
@@ -425,3 +256,10 @@ function draw() {
  * 5 items in wave function :: entropy = 5
  * n items in wave function :: entropy = n (n > 1)
  */
+
+module.exports = { 
+    grid, DIMENSION, INITIAL_WAVE, WAVE_ARRAY,
+    constraints, FACING_DOWN, FACING_LEFT, FACING_RIGHT, FACING_UP,
+    WAVE_STATES, populateWaveStates, initGrid, text, printGridState,
+    waveFunctionCollapse,
+};
